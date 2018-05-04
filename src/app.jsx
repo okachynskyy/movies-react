1fdf782db096f292d544bd5b07b277038fbdd264
Oleksiy Kachynskyy <oleksiy.kachynskyy@gmail.com>
2018-05-04 16:46:33 +0300

    add draft version of movie details page

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import { SearchLayout } from './layouts/search';
+import {SearchLayout} from './layouts/search';
+import {DetailsLayout} from './layouts/details';
 import './app.scss';
 
 export class App extends React.PureComponent {
   render() {
     return (
       <React.Fragment>
-        <SearchLayout />
+        <DetailsLayout/>
       </React.Fragment>
     );
   }
---
+++ src/layouts/details.jsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import {Header} from '../components/header';
+import {Content} from '../components/content';
+import {Footer} from '../components/footer';
+
+import {MovieDetails} from '../components/movie-details';
+import {SubHeader} from '../components/sub-header';
+import {MovieList} from '../components/movie-list'
+
+import './details.scss';
+
+export class DetailsLayout extends React.Component {
+  constructor() {
+    super();
+    this.genre = 'Dramma';
+
+    this.movies = [
+      {
+        poster_path: 'https://image.tmdb.org/t/p/w500/6o0UWX2naW7HK45PDNYmoMIk5qs.jpg',
+        title: 'No Country for Old Men',
+        release_date: '2007',
+        genres: ['Crime', 'Drama', 'Thriller']
+      }, {
+        poster_path: 'https://image.tmdb.org/t/p/w500/6o0UWX2naW7HK45PDNYmoMIk5qs.jpg',
+        title: 'No Country for Old Men',
+        release_date: '2007',
+        genres: ['Crime', 'Drama', 'Thriller']
+      }, {
+        poster_path: 'https://image.tmdb.org/t/p/w500/6o0UWX2naW7HK45PDNYmoMIk5qs.jpg',
+        title: 'No Country for Old Men',
+        release_date: '2007',
+        genres: ['Crime', 'Drama', 'Thriller']
+      }, {
+        poster_path: 'https://image.tmdb.org/t/p/w500/6o0UWX2naW7HK45PDNYmoMIk5qs.jpg',
+        title: 'No Country for Old Men',
+        release_date: '2007',
+        genres: ['Crime', 'Drama', 'Thriller']
+      }
+    ];
+
+    this.movie = {
+      "title": "No Country for Old Men",
+      "tagline": "There are no clean getaways.",
+      "vote_average": 7.8,
+      "release_date": "2007",
+      "poster_path": "https://image.tmdb.org/t/p/w500/6o0UWX2naW7HK45PDNYmoMIk5qs.jpg",
+      "overview": "Llewelyn Moss stumbles upon dead bodies, $2 million and a hoard of heroin in a T" +
+          "exas desert, but methodical killer Anton Chigurh comes looking for it, with loca" +
+          "l sheriff Ed Tom Bell hot on his trail. The roles of prey and predator blur as t" +
+          "he violent pursuit of money and justice collide.",
+      "genres": [
+        "Crime", "Drama", "Thriller"
+      ],
+      "runtime": 122
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+
+        <Header>
+          <MovieDetails movie={this.movie}/>
+          <SubHeader>
+            Films by {this.genre} genre
+          </SubHeader>
+        </Header>
+
+        <Content>
+          <MovieList movies={this.movies}/>
+        </Content>
+
+        <Footer/>
+
+      </React.Fragment>
+    );
+  }
+}
