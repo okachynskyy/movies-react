93cb09aa979ba26c8918a3a89b057a7f9f004021
Oleksiy Kachynskyy <oleksiy.kachynskyy@gmail.com>
2018-05-15 17:32:52 +0300

    Add snap test for app component

---
+++ src/app.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { App } from './app';
+import renderer from 'react-test-renderer';
+
+jest.mock('./app.scss', () => ({}));
+jest.mock('./components/error-boundary');
+jest.mock('./layouts/search');
+
+describe('App', () => {
+  it('renders', () => {
+    const tree = renderer
+      .create(<App />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
