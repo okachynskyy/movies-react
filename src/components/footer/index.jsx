77bc0071613790a26419d77272ee3acef6586a8d
Oleksiy Kachynskyy <oleksiy.kachynskyy@gmail.com>
2018-05-15 17:47:39 +0300

    Refactor Footer component, add mocks

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,1 @@
-import * as React from "react";
-import { SiteName } from '../site-name';
-
-export function Footer() {
-  return (
-    <footer className="main-footer">
-      <div className="container">
-        <SiteName />
-      </div>
-    </footer>
-  );
-}
+export { Footer } from './footer';
