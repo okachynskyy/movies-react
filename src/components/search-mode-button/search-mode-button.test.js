9ef938c8a8a83eddd592f1033013b0463fb1cc49
Oleksiy Kachynskyy <oleksiy.kachynskyy@gmail.com>
2018-05-15 17:22:20 +0300

    Update tests for search-mode-button

--- src/components/search-mode-button/search-mode-button.test.js
+++ src/components/search-mode-button/search-mode-button.test.js
@@ -5,9 +5,16 @@ import renderer from 'react-test-renderer';
 jest.mock('../button/button');
 
 describe('SearchModeButton', () => {
-  it('renders', () => {
+  it('renders red button for active mode', () => {
     const tree = renderer
-      .create(<SearchModeButton />)
+      .create(<SearchModeButton active={true} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders gray button for non active mode', () => {
+    const tree = renderer
+      .create(<SearchModeButton active={false} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
