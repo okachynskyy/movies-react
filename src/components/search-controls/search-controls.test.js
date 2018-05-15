0a5068eb61bc67079a70f0698799e784a9f4be7d
Oleksiy Kachynskyy <oleksiy.kachynskyy@gmail.com>
2018-05-15 17:15:31 +0300

    Add snap test for search-filters

--- src/components/search-controls/search-controls.test.js
+++ src/components/search-controls/search-controls.test.js
@@ -3,7 +3,7 @@ import { SearchControls } from './index';
 import renderer from 'react-test-renderer';
 
 jest.mock('../button/button');
-jest.mock('../search-filters');
+jest.mock('../search-filters/search-filters');
 
 describe('SearchControls', () => {
   it('renders', () => {
